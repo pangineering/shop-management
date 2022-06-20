4f98a9ddf9ad63bdd46ce4005016240e68132546
pantkdkrt <p.engineering6415@gmail.com>
2022-06-20 13:01:45 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,7 +1,28 @@
 const express = require('express')
 require('dotenv').config()
-const port = process.env.PORT || 5000
+const colors = require('colors');
+const cors = require('cors');
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
 
 const app = express()
+
+// Connect to database
+connectDB();
+
+
+app.use(cors());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })
+);
 
 app.listen(port,comsole.log(`Server running on port ${port}`))
