11f1238231d6887b032134688c7738616b3ee61e
pantkdkrt <p.engineering6415@gmail.com>
2022-06-20 16:45:57 +0700

    Update SettingScreen.js

--- client/src/Screens/SettingScreen.js
+++ client/src/Screens/SettingScreen.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Container, Button } from "react-bootstrap"
 
 function SettingScreen() {
   return (
-    <div>SettingScreen</div>
+    <Container>
+      <h1>My Setting</h1>
+      <Container>
+        <h2>Personal</h2>
+        <Button>Save</Button>
+        <hr />
+      </Container>
+      <Container>
+        <h2>Account</h2>
+        <Button>Save</Button>
+        <hr />
+      </Container>
+      <Container>
+        <h2>Shops</h2>
+        <Button>Save</Button>
+        <hr />
+      </Container>
+    </Container>
   )
 }
 
